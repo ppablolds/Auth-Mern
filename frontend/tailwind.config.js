4bdac2917368f726c491031b78a06a6639306545
ppablolds <pablosilvaaluno@outlook.com>
2025-01-17 13:09:57 -0300

    Add colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"P-Federal-blue": "#03045E",
+				"P-Marian-blue": "#023E8A",
+				"P-Honolulu-Blue": "#0077B6",
+				"P-Blue-Green": "#0096C7",
+				"P-Pacific-cyan": "#00B4D8",
+				"P-Vivid-sky-blue": "#48CAE4",
+				"P-Non-Photo-blue-Dark": "#90E0EF",
+				"P-Non-Photo-blue-Light": "#ADE8F4",
+				"P-Light-cyan": "#CAF0F8",
+			}
+		},
 	},
 	plugins: [],
 };
