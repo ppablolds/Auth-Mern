9494ad8923568adb14b2e190ae23e7cb2d150d12
ppablolds <pablosilvaaluno@outlook.com>
2025-01-17 13:11:06 -0300

    feat: Change colors the pages of Authentication

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -21,10 +21,10 @@ const LoginPage = () => {
 			initial={{ opacity: 0, y: 20 }}
 			animate={{ opacity: 1, y: 0 }}
 			transition={{ duration: 0.5 }}
-			className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden'
+			className='max-w-md w-full bg-P-Federal-blue bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden'
 		>
 			<div className='p-8'>
-				<h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text'>
+				<h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-P-Vivid-sky-blue to-P-Blue-Green text-transparent bg-clip-text'>
 					Welcome Back
 				</h2>
 
@@ -46,7 +46,7 @@ const LoginPage = () => {
 					/>
 
 					<div className='flex items-center mb-6'>
-						<Link to='/forgot-password' className='text-sm text-green-400 hover:underline'>
+						<Link to='/forgot-password' className='text-sm text-P-Vivid-sky-blue hover:underline'>
 							Forgot password?
 						</Link>
 					</div>
@@ -55,7 +55,7 @@ const LoginPage = () => {
 					<motion.button
 						whileHover={{ scale: 1.02 }}
 						whileTap={{ scale: 0.98 }}
-						className='w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200'
+						className='w-full py-3 px-4 bg-gradient-to-r from-P-Blue-Green to-P-Honolulu-Blue text-white font-bold rounded-lg shadow-lg hover:from-P-Honolulu-Blue hover:to-P-Marian-blue focus:outline-none focus:ring-2 focus:ring-P-Blue-Green focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200'
 						type='submit'
 						disabled={isLoading}
 					>
@@ -63,10 +63,10 @@ const LoginPage = () => {
 					</motion.button>
 				</form>
 			</div>
-			<div className='px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center'>
+			<div className='px-8 py-4 bg-P-Federal-blue bg-opacity-50 flex justify-center'>
 				<p className='text-sm text-gray-400'>
 					Don't have an account?{" "}
-					<Link to='/signup' className='text-green-400 hover:underline'>
+					<Link to='/signup' className='text-P-Vivid-sky-blue hover:underline'>
 						Sign up
 					</Link>
 				</p>
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -29,11 +29,11 @@ const SignUpPage = () => {
 			initial={{ opacity: 0, y: 20 }}
 			animate={{ opacity: 1, y: 0 }}
 			transition={{ duration: 0.5 }}
-			className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl 
+			className='max-w-md w-full bg-P-Federal-blue bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl 
 			overflow-hidden'
 		>
 			<div className='p-8'>
-				<h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text'>
+				<h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-P-Vivid-sky-blue to-P-Blue-Green text-transparent bg-clip-text'>
 					Create Account
 				</h2>
 
@@ -63,9 +63,8 @@ const SignUpPage = () => {
 					<PasswordStrengthMeter password={password} />
 
 					<motion.button
-						className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
-						font-bold rounded-lg shadow-lg hover:from-green-600
-						hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
+						className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-P-Blue-Green to-P-Honolulu-Blue text-white 
+						font-bold rounded-lg shadow-lg hover:from-P-Honolulu-Blue hover:to-P-Marian-blue focus:outline-none focus:ring-2 focus:ring-P-Blue-Green focus:ring-offset-2
 						 focus:ring-offset-gray-900 transition duration-200'
 						whileHover={{ scale: 1.02 }}
 						whileTap={{ scale: 0.98 }}
@@ -76,10 +75,10 @@ const SignUpPage = () => {
 					</motion.button>
 				</form>
 			</div>
-			<div className='px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center'>
+			<div className='px-8 py-4 bg-P-Federal-blue bg-opacity-50 flex justify-center'>
 				<p className='text-sm text-gray-400'>
 					Already have an account?{" "}
-					<Link to={"/login"} className='text-green-400 hover:underline'>
+					<Link to={"/login"} className='text-P-Vivid-sky-blue hover:underline'>
 						Login
 					</Link>
 				</p>
